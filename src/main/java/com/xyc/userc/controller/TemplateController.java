8f8c9021e702e3c6f5dba43ec68b2a3487eac51e
aveishriety <aveishriety@outlook.com>
2020-08-11 18:07:05 +0800

    s

--- src/main/java/com/xyc/userc/controller/TemplateController.java
+++ src/main/java/com/xyc/userc/controller/TemplateController.java
@@ -13,22 +13,22 @@ import java.util.Map;
  * Created by 1 on 2020/8/11.
  */
 @RestController
-public class TemplateController
-{
-    @GetMapping("/mes/template")
-    public ReturnData templateSend() {
-        // oPh4us7phJe_qUG8o1TGY4Mrd2Yg 氕氘氚
-        // oPh4us_Fe88F-HgRKn9QAYQTBzOs Pluto
+public class TemplateController {
+	@GetMapping("/mes/template")
+	public ReturnData templateSend() {
+		// oPh4us7phJe_qUG8o1TGY4Mrd2Yg 氕氘氚
+		// oPh4us_Fe88F-HgRKn9QAYQTBzOs Pluto
+		// oPh4us__A3ShBKk7fkxu2ehE_OWI 喂喂
 
-        Map<String, String> propsMap = new HashMap<String, String>();
-        propsMap.put("TOUSER", "oPh4us_Fe88F-HgRKn9QAYQTBzOs");
-        propsMap.put("TEMPLATE_ID", "CMzvZBeSWTAYG7qp7B8CwTvc2tT4fwNQO8cQynQ2OCA");
-        propsMap.put("FIRST", "测试数据");
-        propsMap.put("KEYWORD1", "测试数据");
-        propsMap.put("KEYWORD2", "测试数据");
-        propsMap.put("KEYWORD3", "测试数据");
-        propsMap.put("KEYWORD4", "测试数据");
-        propsMap.put("REMARK", "测试数据");
-        return Template.send(propsMap);
-    }
+		Map<String, String> propsMap = new HashMap<String, String>();
+		propsMap.put("TOUSER", "oPh4us_Fe88F-HgRKn9QAYQTBzOs");
+		propsMap.put("TEMPLATE_ID", "CMzvZBeSWTAYG7qp7B8CwTvc2tT4fwNQO8cQynQ2OCA");
+		propsMap.put("FIRST", "测试数据");
+		propsMap.put("KEYWORD1", "测试数据");
+		propsMap.put("KEYWORD2", "测试数据");
+		propsMap.put("KEYWORD3", "测试数据");
+		propsMap.put("KEYWORD4", "测试数据");
+		propsMap.put("REMARK", "测试数据");
+		return Template.send(propsMap);
+	}
 }
