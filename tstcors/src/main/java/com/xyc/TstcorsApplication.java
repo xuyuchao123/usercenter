e2a4d265a89bdc024791d839ba97dbe62877aa63
xyc <949001430@qq.com>
2020-10-17 10:01:17 +0800

    2020 10 16

---
+++ tstcors/src/main/java/com/xyc/TstcorsApplication.java
@@ -0,0 +1,13 @@
+package com.xyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TstcorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TstcorsApplication.class, args);
+	}
+
+}
---
+++ tstcors/src/test/java/com/xyc/TstcorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TstcorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
