17622871c757b19817e52427d5bbfc85ff44740b
xyc <949001430@qq.com>
2020-10-29 09:55:09 +0800

    2020 10 29

--- src/main/java/com/xyc/userc/vo/EnvInfoVo.java
+++ src/main/java/com/xyc/userc/vo/EnvInfoVo.java
@@ -14,23 +14,49 @@ public class EnvInfoVo implements Serializable
 {
     private static final long serialVersionUID = 1L;
 
+    @ApiModelProperty(value="数据编号", position=0)
     private Integer id;
 
-    @ApiModelProperty(value="业务部门", example="综合销售处", position=1)
+    @ApiModelProperty(value="业务部门", position=1)
     private String bumen;
 
+    @ApiModelProperty(value="车牌号", position=2)
     private String carNumber;
+
+    @ApiModelProperty(value="车辆识别代号", position=3)
     private String carVinNumber;
+
+    @ApiModelProperty(value="发动机号", position=4)
     private String engineNumber;
+
+    @ApiModelProperty(value="排放阶段", position=5)
     private String paif;
+
+    @ApiModelProperty(value="通行时间", position=6)
     private String crossTime;
+
+    @ApiModelProperty(value="预报时间", position=7)
     private String forecastTime;
+
+    @ApiModelProperty(value="注册日期", position=8)
     private String zcrq;
+
+    @ApiModelProperty(value="出入类型", position=9)
     private String carInOutString;
+
+    @ApiModelProperty(value="通行门卫", position=10)
     private String entranceName;
+
+    @ApiModelProperty(value="货物名称", position=11)
     private String hwmc;
+
+    @ApiModelProperty(value="运输量", position=12)
     private String ysl;
+
+    @ApiModelProperty(value="车队名称", position=13)
     private String cdmc;
+
+    @ApiModelProperty(value="车辆图片url", position=14)
     private String vehicleUrl;
 
 
