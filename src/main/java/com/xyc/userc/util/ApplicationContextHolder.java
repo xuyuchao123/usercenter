e972237074acc18f09ce77323fbb508ccf809d77
xyc <949001430@qq.com>
2020-11-05 11:34:27 +0800

    2020 11 05

--- src/main/java/com/xyc/userc/util/ApplicationContextHolder.java
+++ src/main/java/com/xyc/userc/util/ApplicationContextHolder.java
@@ -3,10 +3,12 @@ package com.xyc.userc.util;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by 1 on 2020/11/5.
  */
+@Component
 public class ApplicationContextHolder implements ApplicationContextAware
 {
     private static ApplicationContext ac;
