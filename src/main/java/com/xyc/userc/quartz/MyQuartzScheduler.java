58878452f8ea3743b25b0731fa987c45dc9f7975
xyc <949001430@qq.com>
2020-11-06 14:38:17 +0800

    2020 11 06

--- src/main/java/com/xyc/userc/quartz/MyQuartzScheduler.java
+++ src/main/java/com/xyc/userc/quartz/MyQuartzScheduler.java
@@ -118,7 +118,7 @@ public class MyQuartzScheduler
         // 通过JobBuilder构建JobDetail实例，JobDetail规定只能是实现Job接口的实例，JobDetail 是具体Job实例
         JobDetail jobDetail = JobBuilder.newJob(BlackListInOutJob.class).withIdentity("job1", "group1").build();
         // 基于表达式构建触发器
-        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("0 0/2 * * * ? *");
+        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("0 0/15 * * * ? *");
         // CronTrigger表达式触发器 继承于Trigger，TriggerBuilder 用于构建触发器实例
         CronTrigger cronTrigger = TriggerBuilder.newTrigger().withIdentity("job1", "group1")
                 .withSchedule(cronScheduleBuilder).build();
