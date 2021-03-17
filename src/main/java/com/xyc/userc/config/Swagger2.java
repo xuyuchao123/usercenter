5805e1acd12b7c671cf44c0aeaf7da41ae5fd5d4
xyc <949001430@qq.com>
2021-03-17 08:43:43 +0800

    2021 3 16

--- src/main/java/com/xyc/userc/config/Swagger2.java
+++ src/main/java/com/xyc/userc/config/Swagger2.java
@@ -4,13 +4,19 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 1 on 2020/7/10.
@@ -26,13 +32,23 @@ public class Swagger2
     @Bean
     public Docket createRestApi()
     {
+        //在配置好的配置类中增加此段代码即可
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        ticketPar.name("Authorization").description("登录校验")             //name表示名称，description表示描述
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).defaultValue("Bearer ").build();           //required表示是否必填，defaultvalue表示默认值
+        pars.add(ticketPar.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 // 为当前包路径
                 .apis(RequestHandlerSelectors.basePackage("com.xyc.userc")).paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(pars);
     }
+
     // 构建 api文档的详细信息函数,注意这里的注解引用的是哪个
     private ApiInfo apiInfo()
     {
