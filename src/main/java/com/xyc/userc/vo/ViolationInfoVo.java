a6db8ceff6c98ad1869d21eef2d8d7c59f8c8e9c
xyc <949001430@qq.com>
2021-04-06 10:42:52 +0800

    2021 4 6

--- src/main/java/com/xyc/userc/vo/ViolationInfoVo.java
+++ src/main/java/com/xyc/userc/vo/ViolationInfoVo.java
@@ -1,24 +1,44 @@
 package com.xyc.userc.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 
 /**
  * Created by 1 on 2021/4/1.
  */
+@ApiModel(value="违章信息返回对象类型")
 public class ViolationInfoVo implements Serializable
 {
     private static final long serialVersionUID = 1L;
 
+    @ApiModelProperty(value="开单部门", position=0)
     private String billingDepartment;
+
+    @ApiModelProperty(value="开单时间", position=1)
     private Long billingTime;
+
+    @ApiModelProperty(value="开单人", position=2)
     private String openSingle;
+
+    @ApiModelProperty(value="开单序号", position=3)
     private String billingSerialNumber;
+
+    @ApiModelProperty(value="罚款事由", position=4)
     private String reasonForFine;
+
+    @ApiModelProperty(value="罚款金额", position=5)
     private String theAmountOfTheFine;
+
+    @ApiModelProperty(value="支付状态", position=6)
     private String paymentStatus;
 
+    @ApiModelProperty(value="违章图片路径", position=7)
+    private String illegalPictures;
+
     public ViolationInfoVo(String billingDepartment, Long billingTime, String openSingle, String billingSerialNumber,
-                           String reasonForFine, String theAmountOfTheFine, String paymentStatus) {
+                           String reasonForFine, String theAmountOfTheFine, String paymentStatus, String illegalPictures) {
         this.billingDepartment = billingDepartment;
         this.billingTime = billingTime;
         this.openSingle = openSingle;
@@ -26,6 +46,7 @@ public class ViolationInfoVo implements Serializable
         this.reasonForFine = reasonForFine;
         this.theAmountOfTheFine = theAmountOfTheFine;
         this.paymentStatus = paymentStatus;
+        this.illegalPictures = illegalPictures;
     }
 
     public String getBillingDepartment() {
@@ -82,5 +103,13 @@ public class ViolationInfoVo implements Serializable
 
     public void setPaymentStatus(String paymentStatus) {
         this.paymentStatus = paymentStatus;
+    }
+
+    public String getIllegalPictures() {
+        return illegalPictures;
+    }
+
+    public void setIllegalPictures(String illegalPictures) {
+        this.illegalPictures = illegalPictures;
     }
 }
