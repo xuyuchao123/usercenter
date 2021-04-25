11222d154075b5274515669e61877475d11ad55d
xyc <949001430@qq.com>
2021-04-25 14:32:37 +0800

    2021 4 23

---
+++ src/main/java/com/xyc/userc/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.xyc.userc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Created by 1 on 2021/4/25.
+ */
+@Configuration
+public class RedisConfig
+{
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Bean
+    public RedisTemplate redisTemplateInit()
+    {
+
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+        return redisTemplate;
+    }
+}
